96e7d5656161d04b01ced085284ee84f77d9026a
Igor <igorone23@yandex.ru>
2025-08-23 02:02:33 +0300

    Add Telegram bot handlers: /start and /menu commands with WebApp integration

---
+++ bot.js
@@ -0,0 +1,34 @@
+const { Telegraf } = require('telegraf');
+
+const WEBAPP_URL = process.env.WEBAPP_URL || "https://fitness-bot-miniapp.vercel.app/"; // мини-приложение
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start(async (ctx) => {
+  const kb = {
+    reply_markup: {
+      keyboard: [[{ text: "Фитнес меню", web_app: { url: WEBAPP_URL } }]],
+      resize_keyboard: true,
+      is_persistent: true,
+    },
+  };
+  await ctx.reply(
+    "Привет! Я персональный тренер.\nОткрывай меню, чтобы выбрать тренировки.",
+    kb
+  );
+});
+
+bot.command("menu", async (ctx) => {
+  const ikb = {
+    reply_markup: {
+      inline_keyboard: [[{ text: "Открыть фитнес-меню", web_app: { url: WEBAPP_URL } }]],
+    },
+  };
+  await ctx.reply("Открой фитнес-меню:", ikb);
+});
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
